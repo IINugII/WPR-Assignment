775d4d8461b41a9dc84f6a3594035f6c743b76f5
Joshua Ramalho <joshuaramalho437@gmail.com>
2025-05-12 12:53:49 +0200

    Code for pageRoutes and ejs files. Touched on some js files aswell.

--- community-portal-prefilled (1)/routes/pageRoutes.js
+++ community-portal-prefilled (1)/routes/pageRoutes.js
@@ -3,19 +3,33 @@
 const express = require('express');
 const router = express.Router();
 
+const aboutData = require('../data/aboutData');
+const eventsData = require('../data/eventsData');
+const contactMessages = [];
+
 router.get('/', (req, res) => {
+     res.render('home', { upcomingEvents: eventsData.slice(0, 2) });
 });
 
 router.get('/about', (req, res) => {
+    res.render('about', { team: aboutData });
 });
 
 router.get('/events', (req, res) => {
+    res.render('events', { events: eventsData });
 });
 
 router.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
+router.post('/contact', (req, res) => {
+  const { name, email, message } = req.body;
+  contactMessages.push({ name, email, message });
+  res.redirect('/thank-you');
+});
 router.get('/thankyou', (req, res) => {
+    res.render('thank-you');
 });
 
 module.exports = router;
